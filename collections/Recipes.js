25f6410b960bacc495226edb52fc3822aa97de36
Laud Bentil <laud.bentil@meltwater.org>
2015-11-13 14:04:07 +0000

    added schema & autoForm

---
+++ collections/Recipes.js
@@ -0,0 +1,34 @@
+Recipes = new Meteor.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Name"
+  },
+  description: {
+    type: String,
+    label: "Description"
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function(){
+      return this.userId
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  createdAt:{
+    type: Date,
+    label: "Created At",
+    autoValue: function() {
+      return new Date()
+    },
+    autoform: {
+      type: "hidden"
+    }
+  }
+});
+
+Recipes.attachSchema(RecipeSchema);
