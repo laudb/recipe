83a5817ddbe9dc6df7c83797d6861cdeb605110c
Laud Bentil <laud.bentil@meltwater.org>
2015-11-13 15:59:03 +0000

    added template level subscriptions & added sacha:spinner

--- client/Recipes.js
+++ client/Recipes.js
@@ -1,3 +1,12 @@
-Meteor.subscribe('recipes');
+Template.Recipes.onCreated(function(){
+  var self = this;
+  self.autorun(function(){
+    self.subscribe('recipes');
+  });
+});
 
-console.log(Meteor.settings.public.ga.account);
+Template.Recipes.helpers({
+  recipes: ()=> {
+    return Recipes.find({});
+  }
+});
